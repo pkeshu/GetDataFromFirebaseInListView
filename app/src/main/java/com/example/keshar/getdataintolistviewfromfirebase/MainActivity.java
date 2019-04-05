e5199b31dfb2da4a007f74038b09b6f318abbdf5
Keshar <paudelkeshar2055@gmail.com>
2019-04-05 17:29:32 +0545

    Data are accessed from firebase database in list view.

--- app/src/main/java/com/example/keshar/getdataintolistviewfromfirebase/MainActivity.java
+++ app/src/main/java/com/example/keshar/getdataintolistviewfromfirebase/MainActivity.java
@@ -2,12 +2,64 @@ package com.example.keshar.getdataintolistviewfromfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private Firebase firebaseUrl;
+    private ArrayList<String> arrayList=new ArrayList<>();
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView=findViewById(R.id.list_view);
+        Firebase.setAndroidContext(this);
+
+        firebaseUrl=new Firebase("https://applicationdemo-eff34.firebaseio.com/list/");
+
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+
+        mListView.setAdapter(adapter);
+
+        firebaseUrl.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value=dataSnapshot.getValue(String.class);
+                arrayList.add(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
